8410b6557c8ea5335d69f0d2aa563f0e261b7ce9
Ryoma/Cross <ryoma111326@gmail.com>
2024-11-08 11:43:19 +0800

    contact form practice

--- app/page.js
+++ app/page.js
@@ -1,75 +1,51 @@
 'use client'
 
-import React, { useState } from "react";
+
 
 
 
 export default function Page() { 
+  return (
 
-  const [message, setMessage] = useState('');
-  const [submit, setSubmitted] = useState(false);
+  <div className="container p-6">
+    <form className="p-2 bg-black/20 text-white w-60
+    ">
 
-  const [click, setClicked] = useState(false);
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  setSubmitted(true);
-  setMessage('OKOKOK');
-};
-
-const handleAlert = () => {
-  alert('YOU CLICKED MEEEEEEE!!!!')
-  setClicked(true);
-}
-
-return (
-  <>
-  {/* form */}
-  <div className="p-6">
-   <div className="max-w-md mx-auto">
-      <form 
-      className="flex items-center gap-4"
-      onSubmit={handleSubmit}>
+      <h1>Inquiry form</h1>
+      <div>
+        <p>Name</p>
+        <input
+          placeholder="ryoma taguchi"
+          className="border"
+        />
+      </div>
+      <div>
+        <p>Mail Address</p>
         <input 
-          className="flex-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-          placeholder="入力してください"
-        >
-        </input>
-        <button className="hover:text-green-300 hover:bg-black bg-green-500 text-white px-4 py-2 rounded-md">
-          送信
+          className="border"
+        />
+      </div>
+      <div>
+        <p>title</p>
+        <input 
+          className="border"
+        />
+      </div>
+      <div>
+        <p>inquiry</p>
+        <textarea
+          className="border"
+        />
+      </div>
+
+      <div>
+        <button className="rounded-sm bg-blue-300">
+          Send
         </button>
-     </form>
+      </div>
 
-     {setSubmitted && (
-        <div className="">
-          {message}
-        </div>
-     )}
-
-   {/* click */}
-   <div className="py-6">
-      <button 
-        className="border border-black/50 rounded-sm
-         bg-gray-200"
-        onClick={handleAlert}   
-      >
-        click me
-      </button>
-      {setClicked && (
-        <div>
-          YOU CLICKED MEEEEEE OHHHHHHHHH!!!
-        </div>
-      )}
-      
-   </div> 
-
-   </div>
-
-
-
-   </div>
-  </>
-);
-
-
+    </form>
+  </div>
+  
+  )
 }
